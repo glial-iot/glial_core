458f5e725d3fe7407a22eb558cc95dc559699b72
vvzvlad <vvzvlad@gmail.com>
2018-04-26 18:09:29 +0300

    add vaisala support

--- smart_sity_local.lua
+++ smart_sity_local.lua
@@ -169,11 +169,24 @@ local function mqtt_callback(message_id, topic, payload, gos, retain)
       impact_reports:insert{nil, timestamp, subscriptionId, serialNumber, resourcePath, value}
       --print(timestamp, serialNumber, resourcePath, value)
    end
+
+   local _, _, vaisala_sensor_topic = string.find(topic, "(/devices/vaisala/data)")
+   if (vaisala_sensor_topic ~= nil and payload ~= nil) then
+      local vaisala_data = {}
+      _, _, vaisala_data.STATa, vaisala_data.Ta, vaisala_data.RHa, vaisala_data.PAa, vaisala_data.NO2, vaisala_data.SO2, vaisala_data.CO, vaisala_data.H2S, vaisala_data.PM25, vaisala_data.PM10, vaisala_data.STATw, vaisala_data.WD, vaisala_data.WDMAX, vaisala_data.WDMIN, vaisala_data.WS, vaisala_data.WSMAX, vaisala_data.WSMIN, vaisala_data.Tw, vaisala_data.RHw, vaisala_data.PAw, vaisala_data.Ha, vaisala_data.Hd, vaisala_data.Hi, vaisala_data.Ra_mm, vaisala_data.Ra_s, vaisala_data.Ra_mmh  = string.find(payload, "STATa=(%d+%.?%d*), Ta=(%d+%.?%d*)oC, RHa=(%d+%.?%d*)%%, PAa=(%d+%.?%d*)mbar, NO2=(%d+%.?%d*).g/m3, SO2=(%d+%.?%d*).g/m3, CO=(%d+%.?%d*).g/m3, H2S=(%d+%.?%d*).g/m3, PM2.5=(%d+%.?%d*).g/m3, PM10=(%d+%.?%d*).g/m3, STATw=(%d+%.?%d*), WD=(%d+)dir, WDMAX=(%d+)dir, WDMIN=(%d+)dir, WS=(%d+%.?%d*)m/s, WSMAX=(%d+%.?%d*)m/s, WSMIN=(%d+%.?%d*)m/s, Tw=(%d+%.?%d*)oC, RHw=(%d+%.?%d*)%%, PAw=(%d+%.?%d*)mm Hg, Ha=(%d+%.?%d*)mm , Hd=(%d+%.?%d*)s , Hi=(%d+%.?%d*)mm/h, Ra=(%d+%.?%d*)mm, Ra=(%d+%.?%d*)s, Ra=(%d+%.?%d*)mm/h")
+
+      local timestamp = os.time()
+      local resourcePath = "/devices/vaisala/data"
+      local subscriptionId = "internal-vaisala"
+      for value_name, value_data in pairs(vaisala_data) do
+         local value = tonumber(value_data)
+         local serialNumber = value_name
+         impact_reports:insert{nil, timestamp, subscriptionId, serialNumber, resourcePath, value}
+         --print(timestamp, serialNumber, resourcePath, value)
+      end
+
+   end
 end
-
-
-
-
 
 
 box.cfg { listen = 3313, log_level = 4, memtx_dir = "./db", vinyl_dir = "./db", wal_dir = "./db"  }
