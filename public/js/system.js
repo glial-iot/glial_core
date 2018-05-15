268a599d20162366b0c464719fe847977577e3d4
vvzvlad <vvzvlad@gmail.com>
2018-05-15 14:43:01 +0300

    add button color func

--- public/js/system.js
+++ public/js/system.js
@@ -13,6 +13,18 @@ HTMLTableRowElement.prototype.insertCell = (function(oldInsertCell) {
     }
 })(HTMLTableRowElement.prototype.insertCell);
 
+function button_color(button_object, new_class) {
+    new_class = "btn-" + new_class;
+    button_object.removeClass("btn-warning");
+    button_object.removeClass("btn-danger");
+    button_object.removeClass("btn-success");
+    button_object.removeClass("btn-info");
+    button_object.removeClass("btn-primary");
+    button_object.removeClass("btn-secondary");
+    button_object.removeClass("btn-light");
+    button_object.removeClass("btn-dark");
+    button_object.addClass(new_class);
+}
 
 
 var Base64 = { _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode: function(e) { var t = ""; var n, r, i, s, o, u, a; var f = 0;
