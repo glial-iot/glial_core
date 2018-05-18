4954621d56f0d394d06413ca8d646e1c477fd121
vvzvlad <vvzvlad@gmail.com>
2018-05-18 18:36:29 +0300

    rename to private, refactoring

--- bus.lua
+++ bus.lua
@@ -6,7 +6,7 @@ local inspect = require 'inspect'
 local logger = require 'logger'
 
 local bus = {}
-local bus_local = {}
+local bus_private = {}
 local box = box
 local scripts_events = require 'scripts_events'
 local system = require 'system'
@@ -22,7 +22,7 @@ bus.max_seq_value = 0
 bus.avg_seq_value = 0
 bus.current_key = 0
 
-function bus_local.events_handler(topic, value)
+function bus_private.events_handler(topic, value)
    for name, item in pairs(scripts_events) do
       if (item ~= nil and type(item) == "table" and item.topic ~= nil and item.topic == topic) then
          --print("Event "..name.." started on topic "..topic)
@@ -36,7 +36,7 @@ function bus_local.events_handler(topic, value)
    end
 end
 
-function bus_local.get_tsdb_save_attribute(topic)
+function bus_private.get_tsdb_save_attribute(topic)
    local table = bus.bus_storage.index.topic:select(topic, {iterator = 'EQ', limit = 1})
    if (table[1] ~= nil) then
       local tsdb_save = table[1][4]
@@ -48,15 +48,15 @@ function bus_local.get_tsdb_save_attribute(topic)
 end
 
 
-function bus_local.fifo_storage_worker()
+function bus_private.fifo_storage_worker()
    while true do
-      local key, topic, timestamp, value = bus_local.fifo_get_delete_value()
+      local key, topic, timestamp, value = bus_private.fifo_get_delete_topics()
       --print("get fifo value:", key, topic, timestamp, value)
       if (key ~= nil) then
          bus.bus_storage:upsert({topic, timestamp, value}, {{"=", 2, timestamp} , {"=", 3, value}})
          bus.rps_o = bus.rps_o + 1
 
-         local tsdb_save = bus_local.get_tsdb_save_attribute(topic)
+         local tsdb_save = bus_private.get_tsdb_save_attribute(topic)
          if (tsdb_save == true) then
                local answer = influx_storage.update_value("glue", topic, value)
                if (answer ~= nil) then
@@ -71,7 +71,7 @@ function bus_local.fifo_storage_worker()
    end
 end
 
-function bus_local.bus_rps_stat_worker()
+function bus_private.bus_rps_stat_worker()
    while true do
       bus.update_value("/glue/rps_i", bus.rps_i)
       bus.update_value("/glue/rps_o", bus.rps_o)
@@ -82,16 +82,16 @@ function bus_local.bus_rps_stat_worker()
 end
 
 
-function bus_local.add_value_to_fifo_buffer(topic, value)
+function bus_private.add_value_to_fifo_buffer(topic, value)
    local timestamp = os.time()
    if (topic ~= nil and value ~= nil) then
-      local new_value  = bus_local.events_handler(topic, value)
+      local new_value  = bus_private.events_handler(topic, value)
       bus.fifo_storage:insert{nil, topic, timestamp, (new_value or value)}
       bus.rps_i = bus.rps_i + 1
    end
 end
 
-function bus_local.fifo_get_delete_value()
+function bus_private.fifo_get_delete_topics()
    local table = bus.fifo_storage.index.primary:select(nil, {iterator = 'EQ', limit = 1})
    local key, topic, timestamp, value
    if (table[1] ~= nil) then
@@ -109,31 +109,7 @@ function bus_local.fifo_get_delete_value()
    end
 end
 
--------------------Public functions-------------------
-
-function bus.init()
-   bus.fifo_storage = box.schema.space.create('fifo_storage', {if_not_exists = true, temporary = true})
-   bus.fifo_sequence = box.schema.sequence.create("fifo_storage_sequence", {if_not_exists = true})
-   bus.fifo_storage:create_index('primary', {sequence="fifo_storage_sequence", if_not_exists = true})
-
-   bus.bus_storage = box.schema.space.create('bus_storage', {if_not_exists = true})
-   bus.bus_storage:create_index('topic', {parts = {1, 'string'}, if_not_exists = true})
-
-   bus_local.fifo_storage_worker_fiber = fiber.create(bus_local.fifo_storage_worker)
-   bus_local.bus_rps_stat_worker_fiber = fiber.create(bus_local.bus_rps_stat_worker)
-end
-
-function bus.update_value(topic, value) -- external value name (incorrect)
-   bus_local.add_value_to_fifo_buffer(topic, value)
-end
-
-function bus.delete_value(topic)
-   if (topic ~= nil) then
-      bus.bus_storage.index.topic:delete(topic)
-   end
-end
-
-function bus.set_tsdb_save_attribute(topic, value)
+function bus_private.set_tsdb_save_attribute(topic, value)
    if (value ~= nil and (value == true or value == false)) then
       if (topic == "*") then
          for _, tuple in bus.bus_storage.index.topic:pairs() do
@@ -146,11 +122,42 @@ function bus.set_tsdb_save_attribute(topic, value)
    end
 end
 
+function bus_private.delete_topics(topic)
+   if (topic ~= nil) then
+      if (topic == "*") then
+         for _, tuple in bus.bus_storage.index.topic:pairs() do
+            local current_topic = tuple[1]
+            bus.bus_storage.index.topic:delete(current_topic)
+         end
+      else
+         bus.bus_storage.index.topic:delete(topic)
+      end
+   end
+end
+
+
+-------------------Public functions-------------------
+
+function bus.init()
+   bus.fifo_storage = box.schema.space.create('fifo_storage', {if_not_exists = true, temporary = true})
+   bus.fifo_sequence = box.schema.sequence.create("fifo_storage_sequence", {if_not_exists = true})
+   bus.fifo_storage:create_index('primary', {sequence="fifo_storage_sequence", if_not_exists = true})
+
+   bus.bus_storage = box.schema.space.create('bus_storage', {if_not_exists = true})
+   bus.bus_storage:create_index('topic', {parts = {1, 'string'}, if_not_exists = true})
+
+   bus_private.fifo_storage_worker_fiber = fiber.create(bus_private.fifo_storage_worker)
+   bus_private.bus_rps_stat_worker_fiber = fiber.create(bus_private.bus_rps_stat_worker)
+end
+
+function bus.update_value(topic, value) -- external value name (incorrect)
+   bus_private.add_value_to_fifo_buffer(topic, value)
+end
+
 function bus.action_data_handler(req)
    local param_action, param_topic, param_value = req:param("action"), req:param("topic"), req:param("value")
-   if (param_action == "delete_value") then
-      bus.delete_value(param_topic)
-   elseif (param_action == "update_tsdb_attribute") then
+
+   if (param_action == "update_tsdb_attribute") then
       if (param_value == "true") then
          param_value = true
       elseif (param_value == "false") then
@@ -158,12 +165,19 @@ function bus.action_data_handler(req)
       else
          return req:render{ json = { result = false } }
       end
-      bus.set_tsdb_save_attribute(param_topic, param_value)
+      bus_private.set_tsdb_save_attribute(param_topic, param_value)
+
    elseif (param_action == "update_value") then
       if (param_topic == nil or param_value == nil) then
          return req:render{ json = { result = false } }
       end
       bus.update_value(param_topic, param_value)
+
+   elseif (param_action == "delete_topics") then
+      if (param_topic == nil) then
+         return req:render{ json = { result = false } }
+      end
+      bus_private.delete_topics(param_topic)
    end
 
    return req:render{ json = { result = true } }
@@ -173,7 +187,7 @@ function bus.http_data_handler(req)
    local type_item, type_limit = req:param("item"), tonumber(req:param("limit"))
    local return_object
    local data_object, i = {}, 0
-   -- 1(topic), 2(timestamp), 3(value), 4(tsdb_save)
+   --Database struct: 1(topic), 2(timestamp), 3(value), 4(tsdb_save)
    for _, tuple in bus.bus_storage.index.topic:pairs() do
       i = i + 1
       data_object[i] = {}
