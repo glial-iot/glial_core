34143a7fc38e1700607330ba47a56ddec10cf5be
vvzvlad <vvzvlad@gmail.com>
2018-05-21 16:21:13 +0300

    remove user files(only core files in git)

--- user/drivers/example.lua
+++
@@ -1,7 +0,0 @@
-local driver = {}
-driver.name = "Example driver name"
-driver.active = false
-driver.driver_function = function()
-
-end
-return driver
--- user/drivers/fake_off_driver.lua
+++
@@ -1,7 +0,0 @@
-local driver = {}
-driver.name = "fake_off_driver"
-driver.active = true
-driver.driver_function = function()
-a()
-end
-return driver
