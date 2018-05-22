31c5fa98e2f9e749825136a7c5d6a2692f233202
powpilot <unitedthings@yahoo.com>
2018-05-22 15:45:11 +0300

    uploaded the corrected menu.js
    1.changed endpoint url from absolute to relative
    2.removed logging
    3.updated the lines formatting (beautified)

---
+++ public/js/menu.js
@@ -0,0 +1,18 @@
+var dynamenu = new Vue({
+    el: '#dynamenu',
+    data: {
+        menuitems: []
+    },
+
+    created() {
+        fetch('/system_menu_data')
+            .then(response => response.json())
+            .then(json => {
+
+                this.menuitems = json;
+               
+            })
+
+
+    }
+})
