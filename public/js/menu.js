8f2474e842e99cf861b9dbc1fa97bed436f778db
powpilot <unitedthings@yahoo.com>
2018-05-23 21:02:51 +0300

    Vue migration 5/23 (active menu item focus) (#5)
    * removed the extra link to menu.js
    
    * updated menu.js for active item focus
    
    The new menu.js provides the data on what is the currently active view via the "selected" property . The data is provided in the same format as provided by server's json response.
    
    * added the focus on active view
    
    * added the focus on active view
    
    * added the focus on active view
    
    * added the focus on active view
    
    * added the focus on active view
    
    * added the focus on active view

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,15 @@
+var ucurrentpage = location.href;
+var upos = ucurrentpage.search("system_");
+var ulen= ucurrentpage.length;
+
+ucurrentpage = ucurrentpage.slice(upos, ulen);
+ucurrentpage = '/' + ucurrentpage
+
 var dynamenu = new Vue({
     el: '#dynamenu',
     data: {
-        menuitems: []
+        menuitems: [],
+        selected: ucurrentpage
     },
     created() {
         fetch('/system_menu_data')
