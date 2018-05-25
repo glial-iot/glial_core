bca18671a7832f258783e217bb64adaa06c295db
powpilot <unitedthings@yahoo.com>
2018-05-25 11:05:32 +0300

    Add files via upload

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,14 +1,24 @@
-let dynamenu = new Vue({
-    el: '#dynamenu',
-    data: {
-        menuitems: [],
-        current_href: location.pathname
-    },
-    created() {
-        fetch('/system_menu_data')
-            .then(response => response.json())
-            .then(json => {
-                this.menuitems = json;
-            })
-    }
+Vue.component('dynamenu-component', {
+	props: {
+		item: Object,
+		current_href: String
+	},
+	template: '<a v-bind:href="item.href" class="nav-link" v-bind:class="{ active: current_href === item.href }" @click="current_href = item.href"><i v-bind:class="item.icon"></i> {{item.name}}</a>'
+})
+
+
+new Vue({
+	el: '#dynamenu-instance',
+	data: {
+		menuitems: undefined,
+		current_href: location.pathname
+	},
+	created() {
+
+		fetch('http://192.168.1.45:8080/system_menu_data')
+			.then(response => response.json())
+			.then(json => {
+				this.menuitems = json;
+			})
+	}
 })
