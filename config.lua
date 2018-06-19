9df5f3a8dc91504f10cdaba157a526a65322afb1
vvzvlad <vvzvlad@gmail.com>
2018-06-19 14:12:53 +0300

    proto config

--- config.lua
+++ config.lua
@@ -21,4 +21,22 @@ config.MAX_BACKUP_FILES = 20
 config.HTTP_PANEL_PORT = 8080
 
 
+--[[
+config.limit.value = 10
+config.limit.name = "Limit"
+config.limit.description = "Power limit"
+config.limit.type = {}
+config.limit.type.item = config.BUS_LINK
+config.limit.type.type = config.INTEGER
+
+config.limit.type.item = config.PRESETS
+config.limit.type.values = {10, 20, 30}
+
+config.limit.type.item = config.VARIABLE
+config.limit.type.min = 10
+config.limit.type.max = 30 ]]
+
 return config
+
+
+
