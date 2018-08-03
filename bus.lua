5ff05bd0ea58c28bdd11f09d69093ae4161510d6
vvzvlad <vvzvlad@gmail.com>
2018-08-03 16:38:12 +0300

    fix tsdb flag error, add return action result

--- bus.lua
+++ bus.lua
@@ -24,8 +24,7 @@ function bus_private.get_tsdb_attr(topic)
    local tuple = bus.storage.index.topic:get(topic)
 
    if (tuple ~= nil) then
-      local tsdb_save = tuple["tsdb"]
-      if (tsdb_save ~= nil and tsdb_save == true) then
+      if (tuple["tsdb"] ~= nil and tuple["tsdb"] == "true") then
          return true
       end
    end
@@ -75,7 +74,9 @@ function bus_private.add_value_to_fifo_buffer(topic, value)
       local new_value  = scripts_busevents.process(topic, value)
       bus.fifo_storage:insert{nil, topic, tostring((new_value or value)), tonumber(update_time)}
       bus.rps_i = bus.rps_i + 1
+      return true
    end
+   return false
 end
 
 function bus_private.fifo_get_delete_topics() --need refactoring
@@ -97,7 +98,7 @@ function bus_private.fifo_get_delete_topics() --need refactoring
 end
 
 function bus_private.set_tsdb_save_attribute(topic, value)
-   if (value ~= nil and (value == true or value == false)) then
+   if (value ~= nil and (value == "true" or value == "false")) then
       if (topic == "*") then
          for _, tuple in bus.storage.index.topic:pairs() do
             bus.storage:update(tuple["topic"], {{"=", 6, value}})
@@ -105,6 +106,9 @@ function bus_private.set_tsdb_save_attribute(topic, value)
       else
          bus.storage.index.topic:update(topic, {{"=", 6, value}})
       end
+      return true
+   else
+      return false
    end
 end
 
@@ -115,7 +119,9 @@ function bus_private.delete_topics(topic)
       else
          bus.storage.index.topic:delete(topic)
       end
+      return true
    end
+   return false
 end
 
 -------------------Public functions-------------------
@@ -127,7 +133,7 @@ function bus.init()
       {name='update_time',  type='number'},   --3
       {name='type',         type='string'},   --4
       {name='tags',         type='array'},    --5
-      {name='tsdb',         type='string'},   --6
+      {name='tsdb',         type='string'},  --6
    }
    bus.storage = box.schema.space.create('storage', {if_not_exists = true, format = format, id = config.id.bus})
    bus.storage:create_index('topic', {parts = {'topic'}, if_not_exists = true})
@@ -154,7 +160,8 @@ end
 
 
 function bus.update_value(topic, value) -- external value name (incorrect)
-   bus_private.add_value_to_fifo_buffer(topic, value)
+   local result = bus_private.add_value_to_fifo_buffer(topic, value)
+   return result
 end
 
 function bus.get_value(topic)
@@ -199,26 +206,32 @@ function bus.http_api_handler(req)
    local return_object
 
    if (params["action"] == "update_tsdb_attribute") then
-      if (params["value"] == "true") then
-         params["value"] = true
-      elseif (params["value"] == "false") then
-         params["value"] = false
+      if (params["value"] == "true" or params["value"] == "false") then
+         if (params["topic"] ~= nil) then
+            local result = bus_private.set_tsdb_save_attribute(params["topic"], params["value"])
+            return_object = req:render{ json = { result = result } }
+         else
+         return_object = req:render{ json = { result = false, msg = "No valid param topic" } }
+         end
       else
-         return_object = req:render{ json = { result = false } }
+         return_object = req:render{ json = { result = false, msg = "No valid param value" } }
       end
-      bus_private.set_tsdb_save_attribute(params["topic"], params["value"])
 
    elseif (params["action"] == "update_value") then
       if (params["topic"] == nil or params["value"] == nil) then
-         return_object = req:render{ json = { result = false } }
+         return_object = req:render{ json = { result = false, msg = "No valid param topic or value" } }
+      else
+         local result = bus.update_value(params["topic"], params["value"])
+         return_object = req:render{ json = { result = result } }
       end
-      bus.update_value(params["topic"], params["value"])
 
    elseif (params["action"] == "delete_topics") then
       if (params["topic"] == nil) then
-         return_object = req:render{ json = { result = false } }
+         return_object = req:render{ json = { result = false, msg = "No valid param topic" } }
+      else
+         local result = bus_private.delete_topics(params["topic"])
+         return_object = req:render{ json = { result = result } }
       end
-      bus_private.delete_topics(params["topic"])
 
    elseif (params["action"] == "get_bus_serialized") then
       local bus_data = bus.serialize(params["pattern"])
@@ -232,11 +245,13 @@ function bus.http_api_handler(req)
          local topic = tuple["topic"]
          local update_time = tuple["update_time"]
          local value = tuple["value"]
-         local tsdb = tuple["tsdb"]
+         local tsdb
+         if (tuple["tsdb"] == "false") then tsdb = false else tsdb = true end
          local type = tuple["type"]
          local tags = tuple["tags"]
          local diff_time = current_time - update_time
          local diff_time_text = system.format_seconds(diff_time)
+
          if (diff_time > 1) then
             text_time = os.date("%Y-%m-%d, %H:%M:%S", update_time).." ("..(diff_time_text).." ago)"
          else
@@ -252,7 +267,8 @@ function bus.http_api_handler(req)
          return_object = req:render{ json = { none_data = "true" } }
       end
 
-
+   else
+      return_object = req:render{ json = {error = true, error_msg = "Bus API: No valid action"} }
    end
 
    return_object = return_object or req:render{ json = {error = true, error_msg = "Bus API: Unknown error(214)"} }
