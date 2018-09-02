37e26e60dd1b81bf536687ea1d2345785252a3eb
vvzvlad <vvzvlad@gmail.com>
2018-09-02 19:19:47 +0300

    fix error message

--- scripts_timerevents.lua
+++ scripts_timerevents.lua
@@ -190,7 +190,7 @@ function timerevents_private.process()
                if (status ~= true) then
                   returned_data = tostring(returned_data)
                   log_timer_events_error('Timer-event script event "'..script_params.name..'" generate error: '..(returned_data or "")..')', script_params.uuid)
-                  scripts.update({uuid = script_params.uuid, status = scripts.statuses.ERROR, status_msg = 'Driver-event: error: '..(returned_data or "")})
+                  scripts.update({uuid = script_params.uuid, status = scripts.statuses.ERROR, status_msg = 'Shedule-event script error: '..(returned_data or "")})
                end
             end
             scripts_table.counter = scripts_table.period
