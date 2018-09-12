7b32f2f08dcf9eb82f59dd17d86cab7d1018a6ee
Nikolay Matyukhin <matuhin86@gmail.com>
2018-09-12 16:51:47 +0300

    Documentation with vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: 'Начало работы с Glue', link: '/' },
+            { text: 'Документация', link: '/documentation.html' },
+            { text: 'Примеры драйверов', link: '/drivers.html' },
+        ],
+        sidebar:"auto",
+        displayAllHeaders: true,
+        logo: '/logo.png'
+    }
+}
