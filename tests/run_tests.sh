940be75fc2d9630eea04b7936af58c9d7d8c243a
Nikolay Matyukhin <matuhin86@gmail.com>
2018-10-30 14:27:04 +0300

    Changed bash script that runs tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,8 +3,9 @@ cd ..
 today=`date +%Y-%m-%d-%H-%M-%S`
 mkdir -p ./tests/logs
 touch ./tests/logs/tarantool-$today.log
-TARANTOOL_CONSOLE=0 TEST_ENV=1 tarantool glue.lua &> ./tests/logs/tarantool-$today.log &
+TARANTOOL_CONSOLE=0 HTTP_PORT=8889 TARANTOOL_WAL_DIR=test_db tarantool glue.lua &> ./tests/logs/tarantool-$today.log &
 echo "Waiting Tarantool to initialize..."
 sleep 1
 busted ./tests/tests.lua
+rm -rf ./test_db
 kill $!
