ae9df2920e6b3a8f690100037982fe73012e42ab
vvzvlad <vvzvlad@gmail.com>
2018-11-05 14:44:40 +0300

    update deb, add postinst and prerm scripts

--- debian/deb_create.sh
+++ debian/deb_create.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-GIT=`git describe --dirty --always --tags | cut -c 2-`
-VERSION="Version: "$GIT
 
 rm -rf ./temp_deb_packet_create
 
+# Install rocks
 cd ..
 tarantoolctl rocks install http
 tarantoolctl rocks install mqtt
@@ -11,13 +10,9 @@ tarantoolctl rocks install dump
 tarantoolctl rocks install cron-parser
 cd ./debian
 
+# Copy files
 mkdir ./temp_deb_packet_create
 cd ./temp_deb_packet_create
-mkdir -p ./glue/DEBIAN
-
-cp ../control ./glue/DEBIAN/control
-echo $VERSION >> ./glue/DEBIAN/control
-cp ../dirs ./glue/DEBIAN/dirs
 
 mkdir -p ./glue/usr/share/tarantool/glue
 cp ../../*.lua ./glue/usr/share/tarantool/glue
@@ -29,20 +24,33 @@ mkdir -p ./glue/etc/tarantool/instances.enabled/
 rm ./glue/usr/share/tarantool/glue/glue_start.lua
 cp ../wb_instance_glue_start.lua ./glue/etc/tarantool/instances.enabled/glue.lua
 
+# Change owner
 chown -R root:root ./glue/
 chown -R tarantool:tarantool ./glue/etc/tarantool/
 chown -R tarantool:tarantool ./glue/usr/share/tarantool/
 
-KB=`du -sk  ./glue |awk '{print $1}'`
-SIZE="Installed-Size:  "$GIT
+# Make deb metainfo
+mkdir -p ./glue/DEBIAN
 
-echo $SIZE >> ./glue/DEBIAN/control
+cp ../control ./glue/DEBIAN/control
 
+VERSION=`git describe --dirty --always --tags | cut -c 2-`
+VERSION_FOR_CONTROL="Version: "$VERSION
+echo $VERSION_FOR_CONTROL >> ./glue/DEBIAN/control
+
+SIZE=`du -sk  ./glue |awk '{print $1}'`
+SIZE_FOR_CONTROL="Installed-Size: "$SIZE
+echo $SIZE_FOR_CONTROL >> ./glue/DEBIAN/control
+
+cp ../dirs ./glue/DEBIAN/dirs
+cp ../prerm ./glue/DEBIAN/prerm
+cp ../postinst ./glue/DEBIAN/postinst
+
+# Buld
 dpkg-deb --build glue
 
-mv glue.deb ../glue_$GIT.deb
+mv glue.deb ../glue_$VERSION.deb
 
+# Clear
 cd ..
 rm -rf ./temp_deb_packet_create
-
-#lintian glue_0.73-82-gfd80b2b_all.deb
